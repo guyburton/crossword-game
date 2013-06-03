44eca7d9a4f5324b82c8bbf8e9d1f36d85fae021
Guy Burton <guy@beatbutchermpc.com>
2013-06-03 23:10:52 +0100

    Removed non website content

--- src/config.py
+++
@@ -1,6 +0,0 @@
-
-# the size of the crossword grid
-grid_size = 5
-min_word_length = 3
-dictionary = None
-null_char = '.'
--- test/test_utils.py
+++
@@ -1,7 +0,0 @@
-import config
-from grid import Grid
-
-def setWholeGrid(grid, char):
-    for i in range(0, config.grid_size):
-        for j in range(0, config.grid_size):
-            grid.setLetter(i,j, char)
